a5d40a3753b24839439b660be672b6a2507cf71a
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2022-04-20 01:43:25 -0500

    not sure how to get my map to work

--- JS/map.js
+++ JS/map.js
@@ -4,21 +4,48 @@ function initMap(){
   	zoom: 20,
   	center: myLatLng,
 });
-
-    new google.maps.Marker({
+	
+    const carMarker = new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "VOLO Museum",
-    animation: google.maps.Animation.DROP,
-    icon: './photos/impala.png'
+    title: "Volo Museum",
+    icon: "/photos/impala.png"
   });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: "<h1>VOLO</h1><p>Established in 1960 by the Grams family, the Volo Auto Museum is an automobile museum and collector car dealer in the Chicago suburb of Volo, Illinois, USA<p>"
+const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Volo</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  const marker = new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Volo Museum",
   });
 
-  google.maps.event.addListener(marker, 'mouseover', function() {
-    infowindow.open(canvas, marker);
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
   });
 }
+
 window.initMap = initMap;
