c2e8ee743e47442688c9032a5b6ebf0b50e8767c
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2022-04-26 20:22:50 -0500

    finally got it to work !!

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,35 @@
+function init(){
+	//alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(42.327498, -88.171841);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 20,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
 
- function initMap() {
- 	const myLatlng= {lat:42.327498, lng: -88.171841};
-	const map = new google.maps.Map(document.getElementById("map"), {
-  	zoom: 20,
-  	center: myLatlng,
-});
+	var myMap = new google.maps.Map(el, mapOptions);
 
-    const carMarker = new google.maps.Marker({
-    position: myLatlng,
-    map,
-    title: "Volo Museum",
-    icon: "photos/impala.png"
-  });
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'impala.png'
+	});
 
-const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Volo</h1>' +
-    '<div id="bodyContent">' +
-    "<p><b>Volo</b>Established in 1960 by the Grams family, the Volo Auto Museum is an automobile museum and collector car dealer in the Chicago suburb of Volo, Illinois, USA. " + +
-    "</div>" +
-    "</div>";
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-  const marker = new google.maps.Marker({
-    position: myLatlng,
-    map,
-    title: "Volo Museum",
-  });
+	var contentString = '<h2>Volo Museum</h2><p>Established in 1960 by the Grams family, the Volo Auto Museum is an automobile museum and collector car dealer in the Chicago suburb of Volo, Illinois, USA. "</p>';
 
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
-      shouldFocus: false,
-    });
-  });
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
 }
-window.initMap = initMap;
+
+google.maps.event.addDomListener(window, 'load', init);
